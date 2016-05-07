1105873abc9fa89fbce5a1b1d5068cc6f4386989
Trevor Lowe <Trevor Lowe@10.16.21.13>
2016-05-07 14:22:05 -0700

    PaperTest Created

--- ConferenceManager/src/PaperTest.java
+++ ConferenceManager/src/PaperTest.java
@@ -1,4 +1,17 @@
+import org.junit.*;
 
+/**
+ * Runs tests for the Paper class.
+ * 
+ * @author Trevor N. Lowe
+ * @version 1 : 5/5/16
+ */
 public class PaperTest {
 
+	@Before
+	public void setup() {
+		
+	}
+	
+	
 }
