ac9558ef248ab1f164957ccedf2385db955fe876
kevin <shw26@uw.edu>
2016-05-09 02:46:51 -0700

    finish conferenceTest and authorTest

--- ConferenceManager/src/tests/ConferenceTest.java
+++ ConferenceManager/src/tests/ConferenceTest.java
@@ -1,10 +1,18 @@
 package tests;
+
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import model.Author;
+import model.Conference;
+import model.Paper;
+import model.Reviewer;
+import model.SubProgramChair;
+import model.User;
 /**
  * junit test for conference.
  * @author Shao-Han Wang
@@ -12,67 +20,105 @@ import org.junit.Test;
  */
 public class ConferenceTest {
 
-	/*private Conference myConference;
+	private Conference myConference;
+	User first;
+	User second;
+	User third;
+	User fourth;
 	
 	@Before
 	public void setUp(){
-		myConference = new Conference(null, null, null, null, null);
+		first = new User("Jeremy", "Wolf", "jwolf059@uw.edu");
+		second = new User("Samson", "Gefofy", "Starliker123@yahoo.com");
+		third = new User("Jacob", "Grey", "GreyJ@uw.edu");
+		fourth = new User("Ubal", "Sakses", "ZB4LIFE@yahoo.com");
+		ArrayList<User> users = new ArrayList<User>();
+		users.add(first);
+		users.add(second);
+		users.add(third);
+		users.add(fourth);
+		myConference = new Conference("Conference A", first, users, 5);
 	}
 
 	@Test
 	public void getAuthorTest() {
-		assertEquals("getAuthor failed! (not null)", null, myConference.getAuthor());
+		
+		assertEquals("getAuthor failed!", null, myConference.getAuthor());
 	}
 	@Test
 	public void getAuthorListTest() {
-		Author testA = new Author();
+		Author testA = new Author("Wi", "Fi","wifi@everywhere.com", myConference);
 		ArrayList<Author> test = new ArrayList<Author>();
 		test.add(testA);
 		myConference.setAuthorList(test);
-		assertEquals("getAuthorList failed! (not null)", testA, myConference.getAuthorList().get(0));
+		assertEquals("getAuthorList failed!", testA, myConference.getAuthorList().get(0));
 	}
 	@Test
 	public void getPaperListTest() {
-		Paper testP = new Paper(null);
+		Paper testP = new Paper("wifi@everywhere.com");
 		ArrayList<Paper> test = new ArrayList<Paper>();
 		test.add(testP);
 		myConference.setPaperList(test);
-		assertEquals("getPaperList failed! (not null)", testP, myConference.getPaperList().get(0));
+		assertEquals("getPaperList failed!", testP, myConference.getPaperList().get(0));
 	}
 	@Test
 	public void getPCTest() {
-		assertEquals("getPC failed! (not null)", null, myConference.getPC());
+		assertEquals("getPC failed!", "jwolf059@uw.edu", myConference.getPC().getID());
 	}
 	@Test
 	public void getReviewerTest() {
-		assertEquals("getReviewer failed! (not null)", null, myConference.getReviewer());
+		assertEquals("getReviewer failed!", null, myConference.getReviewer());
 	}
 	@Test
 	public void getReviewerListTest() {
-		Reviewer testR = new Reviewer();
+		Reviewer testR = new Reviewer("Wi","Fi","wifi@everywhere.com");
 		ArrayList<Reviewer> test = new ArrayList<Reviewer>();
 		test.add(testR);
 		myConference.setReviewerList(test);
-		assertEquals("getReviewerList failed! (not null)", testR, myConference.getReviewerList().get(0));
+		assertEquals("getReviewerList failed!", testR, myConference.getReviewerList().get(0));
 	}
 	@Test
 	public void getSCTest() {
-		assertEquals("getSC failed! (not null)", null, myConference.getSC());
+		assertEquals("getSC failed!", null, myConference.getSC());
 	}
 	@Test
-	public void getSCListTest() {
-		SubprogramChair testSC = new SubprogramChair();
-		ArrayList<SubprogramChair> test = new ArrayList<SubprogramChair>();
+	public void getSCListTest() { 
+		SubProgramChair testSC = new SubProgramChair("Wi", "Fi", "wifi@everywhere.com", null, null);
+		ArrayList<SubProgramChair> test = new ArrayList<SubProgramChair>();
 		test.add(testSC);
 		myConference.setSCList(test);
-		assertEquals("getSCList failed! (not null)", testSC, myConference.getSCList().get(0));
+		assertEquals("getSCList failed!", testSC, myConference.getSCList().get(0));
 	}
 	@Test
 	public void getThePCTest() {
-		ProgramChair testPC = new ProgramChair();
-		myConference.setThePC(testPC);
-		assertEquals("getThePC failed! (not null)", testPC, myConference.getThePC());
+		assertEquals("getThePC failed!", first.getID(), myConference.getThePC().getID());
 	}
+	/* a test for menu require user input. does not check for any result.
+	@Test
+	public void confMenuTest() {
+		myConference.confMenu(first);
+		
+		//assertEquals(" confMenu failed!", null, myConference);
+	}
+	*/
+	@Test
+	public void getNameTest() {
+		assertEquals("getName failed!", "Conference A", myConference.getName());
+	}
+	@Test
+	public void addPaperTest() {
+		Paper thePaper = new Paper("wifi@everywhere.com");
+		myConference.addPaper(thePaper);
+		assertEquals("addPaper failed!", "wifi@everywhere.com", myConference.getPaperList().get(0).getAuthor());
+	}
+	@Test
+	public void removePaperTest() {
+		Paper thePaper = new Paper("wifi@everywhere.com");
+		myConference.addPaper(thePaper);
+		myConference.removePaper(thePaper);
+		assertEquals("removePaper failed!", true, myConference.getPaperList().isEmpty());
+	}
+	
 	/*
 	@Test
 	public void Test() {
