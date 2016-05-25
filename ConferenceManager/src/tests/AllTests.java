2094e378bfc7ef5d7f9c7bf03b3855677be75417
Jeremy Wolf <jwolf059@uw.edu>
2016-05-25 08:56:05 -0700

    All test

--- ConferenceManager/src/tests/AllTests.java
+++ ConferenceManager/src/tests/AllTests.java
@@ -5,8 +5,16 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AuthorTest.class, ConferenceTest.class, MenuTest.class, PaperTest.class, ProgramChairTest.class,
-		ReviewerTest.class, ReviewTest.class, SubProgramChairTest.class, UserTest.class })
+@SuiteClasses({ 
+	AuthorTest.class, 
+	ConferenceTest.class, 
+	MenuTest.class, 
+	PaperTest.class, 
+	ProgramChairTest.class,
+	ReviewerTest.class, 
+	ReviewTest.class, 
+	SubProgramChairTest.class, 
+	UserTest.class })
 
 public class AllTests {
 
